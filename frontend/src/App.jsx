9673dbf40883fb5013d71478e99a703d4ff25bf3
Shivansh Nigam <s2704nigam@gmail.com>
2025-08-27 12:53:34 +0530

    Setup Frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="h-screen w-screen flex items-center justify-center bg-gradient-to-r from-blue-400 to-purple-500">
+      <h1 className="text-4xl font-bold text-white">
+        Setup Complete!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
